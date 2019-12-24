78a47203a5b3a48fb19b04f2e6b088ac6d031666
FlameDarks <842356712@qq.com>
2019-12-24 16:31:18 +0800

    1

--- first/models.py
+++ first/models.py
@@ -1,5 +1,4 @@
 from django.db import models
 class Book(models.Model):
     name=models.CharField(max_length=100)
-    price=models.FloatField(default=0)
-# Create your models here.
+    price=models.IntegerField(default=0)
