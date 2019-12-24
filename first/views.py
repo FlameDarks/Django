2ae3f8159cfb68e447431d130350463ec36c0775
FlameDarks <842356712@qq.com>
2019-12-24 15:39:57 +0800

    1

--- first/views.py
+++ first/views.py
@@ -1,3 +1,7 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from first.models import Book
 
-# Create your views here.
+def add(request):
+    Book.objects.create(name="红楼梦",price=99)
+    return HttpResponse("添加完成")
