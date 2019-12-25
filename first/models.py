9fc8574a628e55cb14184812d9494b98cbe30aa1
FlameDarks <842356712@qq.com>
2019-12-25 10:57:24 +0800

    1

--- first/models.py
+++ first/models.py
@@ -1,4 +1,9 @@
 from django.db import models
 class Book(models.Model):
-    name=models.CharField(max_length=100)
-    price=models.IntegerField(default=0)
+    name=models.CharField(verbose_name="书名",max_length=100)
+    price=models.IntegerField(verbose_name="价格",default=0)
+    def __str__(self):
+        return self.name
+    class Meta:
+        verbose_name="书籍"
+        verbose_name_plural=verbose_name
